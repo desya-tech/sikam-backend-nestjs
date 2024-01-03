ab27744fa6c759ef817bdef0f1314785cf6e5dc8
desya <desya.tech@gmail.com>
2024-01-03 13:54:22 +0700

    hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,5 +9,10 @@ async function bootstrap() {
   await app.listen(3000, '0.0.0.0', function() {
     console.log('Listening to port: ' + 3000);
   });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
